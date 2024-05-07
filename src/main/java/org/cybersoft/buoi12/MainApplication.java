c3cddb19319baea90e7201fbd0ae57e9de14eb85
Quang Bui <buiminhquang188@gmail.com>
2024-05-07 09:14:16 +0700

    Homework: Day 12 - Remove unused import, update svg

--- src/main/java/org/cybersoft/buoi12/MainApplication.java
+++ src/main/java/org/cybersoft/buoi12/MainApplication.java
@@ -1,13 +1,7 @@
 package org.cybersoft.buoi12;
 
-import org.cybersoft.buoi12.Company.Company;
-import org.cybersoft.buoi12.Director.Director;
-import org.cybersoft.buoi12.Employee.Employee;
-import org.cybersoft.buoi12.Manager.Manager;
 import org.cybersoft.buoi12.User.User;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainApplication {
