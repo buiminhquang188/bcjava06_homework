e0890f8110c841aa6cfed7c18ac67fc77752882b
Quang Bui <buiminhquang188@gmail.com>
2024-07-01 13:58:59 +0700

    Capstone: Add Services.

---
+++ src/main/java/org/cybersoft/capstone/src/main/java/org/cybersoft/capstone/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,6 @@
+package org.cybersoft.capstone.service.impl;
+
+import org.cybersoft.capstone.service.ProjectService;
+
+public class ProjectServiceImpl implements ProjectService {
+}
---
+++ src/main/java/org/cybersoft/capstone/src/main/java/org/cybersoft/capstone/service/impl/TaskServiceImpl.java
@@ -0,0 +1,6 @@
+package org.cybersoft.capstone.service.impl;
+
+import org.cybersoft.capstone.service.TaskService;
+
+public class TaskServiceImpl implements TaskService {
+}
