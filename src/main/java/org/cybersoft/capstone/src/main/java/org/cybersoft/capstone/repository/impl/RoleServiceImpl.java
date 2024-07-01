9d72e18700bfd4f26fa7a12941a301b6726386ea
Quang Bui <buiminhquang188@gmail.com>
2024-07-01 14:07:03 +0700

    Capstone: Remove Service in Repository

--- src/main/java/org/cybersoft/capstone/src/main/java/org/cybersoft/capstone/repository/impl/RoleServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package org.cybersoft.capstone.repository.impl;
-
-import org.cybersoft.capstone.repository.RoleService;
-
-public class RoleServiceImpl implements RoleService {
-}
--- src/main/java/org/cybersoft/capstone/src/main/java/org/cybersoft/capstone/repository/impl/UserServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package org.cybersoft.capstone.repository.impl;
-
-import org.cybersoft.capstone.repository.UserService;
-
-public class UserServiceImpl implements UserService {
-}
