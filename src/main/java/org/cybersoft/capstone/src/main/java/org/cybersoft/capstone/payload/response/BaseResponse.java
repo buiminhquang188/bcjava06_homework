d2ce7cddbf933c7d887f8427ee98416e77c717d5
Quang Bui <buiminhquang188@gmail.com>
2024-07-01 14:03:51 +0700

    Capstone: Add BaseResponse, status enum

---
+++ src/main/java/org/cybersoft/capstone/src/main/java/org/cybersoft/capstone/payload/response/BaseResponse.java
@@ -0,0 +1,37 @@
+package org.cybersoft.capstone.payload.response;
+
+public class BaseResponse<T> {
+    private Integer statusCode;
+    private String message;
+    private T data;
+
+    public BaseResponse(Integer statusCode, String message, T data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
